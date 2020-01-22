f8880364be05fb85c3ae88e01f3c8cebd779bc7d
Amanda Clement <amanda.clement22@hotmail.com>
2020-01-21 22:25:24 -0500

    Ex1: Changing hard-coded numbers (colors) to variables

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -11,6 +11,7 @@ Edited by Amanda Clement
 // Constants
 const numPixels = 1000;
 const delay = 1000;
+const defaultColor = '#000000';
 
 // To rotate the pixels
 let rotation = 0;
@@ -26,8 +27,6 @@ document.addEventListener('keydown', rotate);
 // Adds DIVs to the page along with event listeners that will allow
 // then to change color on mouseover.
 function setup() {
-  console.log('hello');
-
   // A loop that runs once per pixel we need
   for (let i = 0; i < numPixels; i++) {
     // Create a DIV and store it in a variable
@@ -48,17 +47,19 @@ function setup() {
 // Called by the mouseover event handler on each pixel. Changes
 // the pixel's color and sets a timer for it to revert
 function paint(e) {
+  // e.target contains the specific element moused over so let's
+  // save that into a variable for clarity.
+  let pixel = e.target;
+
   // Math.floor to round to nearest integer (downwards)
   // Math.random returns number in range of 0-1
   let r = Math.floor(Math.random() * 255);
   let b = Math.floor(Math.random() * 255);
   let g = Math.floor(Math.random() * 255);
 
-  // e.target contains the specific element moused over so let's
-  // save that into a variable for clarity.
-  let pixel = e.target;
+  let paintedColor = `rgb(${r},${g},${b})`;
   // To choose random painting color each time user paints a pixel
-  pixel.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";;
+  pixel.style.backgroundColor = paintedColor;
 
   // Set a timeout to call the reset function after a delay
   // When we pass additional parameters (like 'pixel' below) they
@@ -70,7 +71,7 @@ function paint(e) {
 //
 // Takes provided pixel element and sets color back to black (bg color)
 function resetPixel(pixel) {
-  pixel.style.backgroundColor = '#000000'; // back to black
+  pixel.style.backgroundColor = defaultColor; // back to black
 }
 
 // remove
@@ -78,7 +79,7 @@ function resetPixel(pixel) {
 // To remove pixel color when user clicks on it
 function remove(e) {
   let pixel = e.target;
-  pixel.style.backgroundColor = '#000000'; // back to black
+  pixel.style.backgroundColor = defaultColor; // back to black
 }
 
 // rotate
