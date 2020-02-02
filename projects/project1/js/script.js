b67b9a92520b3ca94cffef5b86783ec947a4e07a
Amanda Clement <amanda.clement22@hotmail.com>
2020-02-02 13:52:09 -0500

    P1: Making options draggable/droppable

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -7,5 +7,34 @@ Amanda Clement
 
 *********************************************************************/
 
+// sounds effects will go here as constants
+
+let $heart;
+let $face;
+
 // When the document is loaded, we call the setup function
 $(document).ready(setup);
+
+function setup() {
+  $heart = $("#heart");
+  $face = $("#face");
+
+  $heart.draggable({
+    // reverts back to original position when dropped
+    // revert: true,
+    // putting clone (placeholder) in place so that only a copy of img is dragged
+    // as if original image is fixed
+    helper: "clone",
+    // contain within container div to remove scroll
+    containment: "#containment-wrapper",
+    scroll: false
+  });
+
+  $face.droppable({
+    drop: onDrop
+  });
+}
+
+function onDrop(event, ui) {
+
+}
