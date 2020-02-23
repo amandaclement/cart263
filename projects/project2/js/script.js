ebad1c746ff2babb2423f411f72d7fad42739912
Amanda Clement <amanda.clement22@hotmail.com>
2020-02-23 11:14:58 -0500

    P2: Added starting code

---
+++ projects/project2/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/********************************************************************
+
+Project 2
+Amanda Clement
+
+*********************************************************************/
+
+// When the document is loaded, we call the setup function
+$(document).ready(setup);
+
+// setup()
+//
+// This code will run when the document is ready
+function setup() {
+
+}
