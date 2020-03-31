4409babe4f9e55df3418bc3e5277afd886a10dcb
Amanda Clement <amanda.clement22@hotmail.com>
2020-03-31 10:57:53 -0400

    P3: Started project 3

---
+++ projects/project3/js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/*****************
+
+Project 3
+Amanda Clement
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
