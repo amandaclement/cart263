6fe0c1d020100b647cc8396df18555d0f1ec14d0
Amanda Clement <amanda.clement22@hotmail.com>
2020-03-31 11:35:58 -0400

    P3: Creating mars

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -10,12 +10,22 @@ author, and this description to match your project!
 
 ******************/
 
+// Planet texture images
+let marsTextureImg;
+
+// Sounds effects
+let marsSFX;
+
 // preload()
 //
 // Description of preload
 
 function preload() {
+  // Loading images
+  marsTextureImg = loadImage('../assets/images/marsTexture.png');
 
+  // Loading sounds for each planet
+  marsSFX = loadSound('../assets/sounds/mars.mp3');
 }
 
 
@@ -24,6 +34,12 @@ function preload() {
 // Description of setup
 
 function setup() {
+  // Working in WEBGL
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  // Black background
+  background(0);
+  // Angles are in degrees
+  angleMode(DEGREES);
 
 }
 
@@ -33,5 +49,38 @@ function setup() {
 // Description of draw()
 
 function draw() {
+  // Drawing mars
+  marsSphere();
+}
 
+function marsSphere() {
+  // Map mouse location (horizontal)
+  let distX = map(mouseX, width / 2, 2, 0, width);
+  // Making it rotate according to mouseX
+  let rotationValue = (frameCount * 0.0001 * distX);
+
+  // Making it rotate across each axis
+  rotateY(rotationValue);
+  rotateX(rotationValue);
+  rotateZ(rotationValue);
+
+  // Removing stroke
+  noStroke();
+  // Apply the appropriate planet texture (img)
+  texture(marsTextureImg);
+
+  // Draw the planet sphere
+  sphere(100, 100, 24, 24);
+}
+
+// mousePressed()
+//
+// User presses mouse to activate sound
+function mousePressed() {
+  // If music is already playing and mouse is pressed again, just keep playing it
+  if (marsSFX.isPlaying()) {
+     marsSFX.playMode('sustain');
+   } else {
+     marsSFX.loop(); // Music starts on first mouse press and loops
+   }
 }
