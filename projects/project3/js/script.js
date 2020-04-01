de2d843e5d3e8e5237b129278df6f57f183f2a70
Amanda Clement <amanda.clement22@hotmail.com>
2020-03-31 20:03:42 -0400

    P3: Adding a bunch of spheres to represent the planets, and added orbitControl

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -19,7 +19,6 @@ let marsSFX;
 // preload()
 //
 // Description of preload
-
 function preload() {
   // Loading images
   marsTextureImg = loadImage('../assets/images/marsTexture.png');
@@ -32,45 +31,102 @@ function preload() {
 // setup()
 //
 // Description of setup
-
 function setup() {
   // Working in WEBGL
   createCanvas(windowWidth, windowHeight, WEBGL);
   // Black background
   background(0);
-  // Angles are in degrees
-  angleMode(DEGREES);
-
 }
 
 
 // draw()
 //
 // Description of draw()
-
 function draw() {
-  // Drawing mars
-  marsSphere();
+  background(0);
+  // Removing the stroke on all planets
+  noStroke();
+
+  // orbitControl is a p5 function allowing the user to drag and move around the scene
+  // up and down scroll controls zoom
+  // click-drag controls perspective angle
+  orbitControl();
+
+  // For now I just created a bunch of copies of mars to visualize the scene
+  push();
+  mars();
+  pop();
+
+  push();
+  translate(p5.Vector.fromAngle(millis() / 6000, 300));
+  mars();
+  pop();
+
+  push();
+  translate(p5.Vector.fromAngle(millis() / 7000, 150));
+  mars();
+  pop();
+
+  push();
+  translate(p5.Vector.fromAngle(millis() / 5000, 450));
+  mars();
+  pop();
+
+  push();
+  translate(p5.Vector.fromAngle(millis() / 9000, 600));
+  mars();
+  pop();
+
+  push();
+  translate(p5.Vector.fromAngle(millis() / 5000, -150));
+  mars();
+  pop();
+
+  push();
+  translate(p5.Vector.fromAngle(millis() / 8000, -300));
+  mars();
+  pop();
+
+  push();
+  translate(p5.Vector.fromAngle(millis() / 9000, -450));
+  mars();
+  pop();
+
+  push();
+  translate(p5.Vector.fromAngle(millis() / 7000, -600));
+  mars();
+  pop();
 }
 
-function marsSphere() {
+function mars() {
+  push();
+
+  // Removing stroke
+  noStroke();
+
+  // Apply the appropriate planet texture (img)
+  // texture(marsTextureImg);
+
   // Map mouse location (horizontal)
   let distX = map(mouseX, width / 2, 2, 0, width);
   // Making it rotate according to mouseX
-  let rotationValue = (frameCount * 0.0001 * distX);
+  let rotationValue = (frameCount * 0.000005 * distX);
 
   // Making it rotate across each axis
   rotateY(rotationValue);
   rotateX(rotationValue);
   rotateZ(rotationValue);
 
-  // Removing stroke
-  noStroke();
-  // Apply the appropriate planet texture (img)
-  texture(marsTextureImg);
+  // Move your mouse to change light direction
+  let dirX = (mouseX / width - 0.5) * 2;
+  let dirY = (mouseY / height - 0.5) * 2;
+
+  // Adding directional light
+  directionalLight(250, 250, 250, -dirX, -dirY, -1);
 
   // Draw the planet sphere
-  sphere(100, 100, 24, 24);
+  sphere(50);
+  pop();
 }
 
 // mousePressed()
@@ -79,8 +135,8 @@ function marsSphere() {
 function mousePressed() {
   // If music is already playing and mouse is pressed again, just keep playing it
   if (marsSFX.isPlaying()) {
-     marsSFX.playMode('sustain');
-   } else {
-     marsSFX.loop(); // Music starts on first mouse press and loops
-   }
+    marsSFX.playMode('sustain');
+  } else {
+    marsSFX.loop(); // Music starts on first mouse press and loops
+  }
 }
