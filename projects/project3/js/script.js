7d5c21c3f3790d63cb76531679cf6c16e91b903d
Amanda Clement <amanda.clement22@hotmail.com>
2020-04-02 14:57:20 -0400

    P3: Adding the planets

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -70,17 +70,17 @@ function preload() {
 
   // Loading sounds for each planet
   // Sounds from https://www.youtube.com/watch?v=IQL53eQ0cNA & https://www.youtube.com/watch?v=UTAPvPLb7t4
-  let sunSFX = loadSound('../assets/sounds/sun.mp3');
-  let mercurySFX = loadSound('../assets/sounds/mercury.mp3');
-  let venusSFX = loadSound('../assets/sounds/venus.mp3');
-  let earthSFX = loadSound('../assets/sounds/earth.mp3');
-  let moonSFX = loadSound('../assets/sounds/moon.mp3');
-  let marsSFX = loadSound('../assets/sounds/mars.mp3');
-  let jupiterSFX = loadSound('../assets/sounds/jupiter.mp3');
-  let saturnSFX = loadSound('../assets/sounds/saturn.mp3');
-  let uranusSFX = loadSound('../assets/sounds/uranus.mp3');
-  let neptuneSFX = loadSound('../assets/sounds/neptune.mp3');
-  let plutoSFX= loadSound('../assets/sounds/pluto.mp3');
+  sunSFX = loadSound('../assets/sounds/sun.mp3');
+  mercurySFX = loadSound('../assets/sounds/mercury.mp3');
+  venusSFX = loadSound('../assets/sounds/venus.mp3');
+  earthSFX = loadSound('../assets/sounds/earth.mp3');
+  moonSFX = loadSound('../assets/sounds/moon.mp3');
+  marsSFX = loadSound('../assets/sounds/mars.mp3');
+  jupiterSFX = loadSound('../assets/sounds/jupiter.mp3');
+  saturnSFX = loadSound('../assets/sounds/saturn.mp3');
+  uranusSFX = loadSound('../assets/sounds/uranus.mp3');
+  neptuneSFX = loadSound('../assets/sounds/neptune.mp3');
+  plutoSFX = loadSound('../assets/sounds/pluto.mp3');
 }
 
 
@@ -90,12 +90,20 @@ function preload() {
 function setup() {
   // Working in WEBGL
   createCanvas(windowWidth, windowHeight, WEBGL);
-  // Black background
-  background(0);
 
   // Creating objects (planets)
   // (radius,texture,millisDivider,length)
-  mars = new Planet(50,marsTextureImg,9000,300);
+  sun = new Planet(60, sunTextureImg, 0, 0);
+  mercury = new Planet(25, mercuryTextureImg, 8000, -150);
+  venus = new Planet(40, venusTextureImg, 6000, 250);
+  earth = new Planet(40, earthTextureImg, 7000, -350);
+  moon = new Planet(20, moonTextureImg, 9000, 450);
+  mars = new Planet(40, marsTextureImg, 5000, -550);
+  jupiter = new Planet(50, jupiterTextureImg, 7000, 650);
+  saturn = new Planet(40, saturnTextureImg, 6000, -750);
+  uranus = new Planet(40, uranusTextureImg, 8000, 850);
+  neptune = new Planet(40, neptuneTextureImg, 5000, -950);
+  pluto = new Planet(30, plutoTextureImg, 9000, 1050);
 }
 
 
@@ -104,21 +112,84 @@ function setup() {
 // Description of draw()
 function draw() {
   background(0);
-  // Removing the stroke on all planets
-  noStroke();
 
   // orbitControl is a p5 function allowing the user to drag and move around the scene
   // up and down scroll controls zoom
   // click-drag controls perspective angle
   orbitControl();
 
-  // Adding planet shadow
-  shadow();
+  // Removing the stroke on all planets
+  noStroke();
 
   // Positioning, rotating, and displaying the planets
+  push();
+  sun.position();
+  sun.rotation();
+  sun.display();
+  pop();
+
+  // Adding shadow
+  shadow();
+
+  push();
+  mercury.position();
+  mercury.rotation();
+  mercury.display();
+  pop();
+
+  push();
+  venus.position();
+  venus.rotation();
+  venus.display();
+  pop();
+
+  push();
+  earth.position();
+  earth.rotation();
+  earth.display();
+  pop();
+
+  push();
+  moon.position();
+  moon.rotation();
+  moon.display();
+  pop();
+
+  push();
   mars.position();
   mars.rotation();
   mars.display();
+  pop();
+
+  push();
+  jupiter.position();
+  jupiter.rotation();
+  jupiter.display();
+  pop();
+
+  push();
+  saturn.position();
+  saturn.rotation();
+  saturn.display();
+  pop();
+
+  push();
+  uranus.position();
+  uranus.rotation();
+  uranus.display();
+  pop();
+
+  push();
+  neptune.position();
+  neptune.rotation();
+  neptune.display();
+  pop();
+
+  push();
+  pluto.position();
+  pluto.rotation();
+  pluto.display();
+  pop();
 }
 
 // mousePressed()
@@ -137,8 +208,8 @@ function draw() {
 // uses p5's directionalLight
 function shadow() {
   // Moving mouse changes light direction
-  let dirX = (mouseX / width - 0.5) * 2;
-  let dirY = (mouseY / height - 0.5) * 2;
+  let dirX = (mouseX / width - 0.5);
+  let dirY = (mouseY / height - 0.5);
 
   // Adding directional light
   directionalLight(250, 250, 250, -dirX, -dirY, -1); // white
@@ -146,7 +217,7 @@ function shadow() {
 
 // Creating the planet
 class Planet {
-  constructor(radius,texture,millisDivider,length) {
+  constructor(radius, texture, millisDivider, length) {
     this.radius = radius; // size
     this.texture = texture; // texture (img)
     this.rotationSpeed = 0.000005;
